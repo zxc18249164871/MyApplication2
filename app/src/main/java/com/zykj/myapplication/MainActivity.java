2d2147c4b2a333dfb60d24e2e36e61ee4d629ec1
ckk2497899433 <2497899433@qq.com>
2017-04-08 10:19:41 +0800

    新项目

---
+++ app/src/main/java/com/zykj/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zykj.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "张晓池", Toast.LENGTH_SHORT).show();
+    }
+}
